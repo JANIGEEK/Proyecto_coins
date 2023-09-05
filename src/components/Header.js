6a1911698ffaba99ee487aaf8471580405a6688e
JANIGEEK <josep_angelo@hotmail.com>
2023-09-04 19:32:48 -0600

    Navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect , useState} from "react";
 
-const Header = () => {
+const Header = ({Search}) => {
+
+  const [search, setSearch]=useState('');
+
+ /*useEffect(()=>{
+  const formSearch=document.getElementById("sub-search");
+  formSearch.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    try{
+      let searchInput=e.target.coinSearch.value;
+
+      setSearch(searchInput);
+    }
+    catch(err){
+      console.log(err);
+    }
+
+  })},[]
+  )*/
+Search=search;
   return (
+    
     <nav className="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
       <div className="container">
         <div className="container-fluid">
@@ -9,16 +29,23 @@ const Header = () => {
             Coin app
           </a>
         </div>
-        <form className="d-flex">
+       {/* <form className="d-flex" id="sub-search">*/}
           <input
             className="form-control me-sm-2"
             type="search"
             placeholder="Search"
+            name="coinSearch"
+            onChange={(e) => {
+              console.log(e.target.value);
+              setSearch(e.target.value)
+              
+            }}
           />
           <button className="btn btn-secondary my-2 my-sm-0" type="submit">
             Search
           </button>
-        </form>
+        {//</form>
+}
       </div>
     </nav>
   );
